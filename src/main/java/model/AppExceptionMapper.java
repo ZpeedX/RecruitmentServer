01277fabdc09561f0a8b41cf9cb3470a2f54ba47
saboo <saboo@kth.se>
2018-03-07 16:20:46 +0100

    added more logging

--- src/main/java/model/AppExceptionMapper.java
+++ src/main/java/model/AppExceptionMapper.java
@@ -22,8 +22,12 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class AppExceptionMapper implements ExceptionMapper<Throwable> {
 
-    static final Logger LOGGER = Logger.getLogger("Server_Logger");
-    FileHandler fh;
+    private static final Logger LOGGER = Logger.getLogger("Server_Logger");
+    private final String SERVER_LOGGER_SEVERE_FILE = "Server_Logger_Severe.log";
+    private final String SERVER_LOGGER_WARNING_FILE = "Server_Logger_Severe.log";
+    private final String SERVER_LOGGER_INFO_FILE = "Server_Logger_Severe.log";
+
+    private FileHandler fh;
 
     /**
      * Error response returned based on exception
@@ -33,7 +37,6 @@ public class AppExceptionMapper implements ExceptionMapper<Throwable> {
      */
     @Override
     public Response toResponse(Throwable ex) {
-        logErrorMsg(ex);
         System.out.println("MAPPER TRIGGERED: ");
 
         if (ex instanceof AppRuntimeException) {
@@ -41,66 +44,78 @@ public class AppExceptionMapper implements ExceptionMapper<Throwable> {
             System.out.println(ex.getMessage());
             switch (ErrorMessageEnum.valueOf(ex.getMessage())) {
                 case INVALID_CONTENT:
+                    logErrorMsg(ex, Level.INFO);
                     System.out.println("INVALID_CONTENT");
                     return Response.status(Response.Status.BAD_REQUEST).build();
                 case CONTENT_PRESENT:
+                    logErrorMsg(ex, Level.INFO);
                     System.out.println("CONTENT_PRESENT");
                     return Response.status(Response.Status.CONFLICT).build();
                 case USERNAME_PRESENT:
+                    logErrorMsg(ex, Level.INFO);
                     System.out.println("USERNAME_PRESENT");
                     return Response.status(Response.Status.CONFLICT)
                             .entity(errorJsonWithCodeString(ErrorMessageConstants.USERNAME_PRESENT_CODE))
                             .type(MediaType.APPLICATION_JSON)
                             .build();
                 case SSN_PRESENT:
-                    System.out.println("SSN_PRESENT");
+                    logErrorMsg(ex, Level.INFO);
                     return Response.status(Response.Status.CONFLICT)
                             .entity(errorJsonWithCodeString(ErrorMessageConstants.SSN_PRESENT_CODE))
                             .type(MediaType.APPLICATION_JSON)
                             .build();
                 case NO_CONTENT:
-                    System.out.println("NO_CONTENT");
+                    logErrorMsg(ex, Level.WARNING);
                     return Response.status(Response.Status.NO_CONTENT).build();
                 case NO_DB_CONNECTION:
+                    logErrorMsg(ex, Level.SEVERE);
                     System.out.println("NO_DB_CONNECTION");
                     return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
                 case OPERTAION_FAILED:
-                    System.out.println("OPERTAION_FAILED");
+                    logErrorMsg(ex, Level.INFO);
                     return Response.status(Response.Status.NO_CONTENT).build();
                 default:
+                    logErrorMsg(ex, Level.SEVERE);
                     System.out.println("DEFUALT");
                     return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
             }
         } else {
-            System.out.println("SOMETHING ELSE HAPPEND");
+
+            logErrorMsg(ex, Level.SEVERE);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
 
     }
-    
-    /**
-     * Converts error code to json format
-     * @param code given error code
-     * @return json string
-     */
+
     private String errorJsonWithCodeString(int code) {
         return "{\"error\":" + code + "}";
     }
-    
-    /**
-     * Logs the given throwble exception into a file
-     * @param ex given throwable exception
-     */
-    private void logErrorMsg(Throwable ex) {
+
+    private void logErrorMsg(Throwable ex, Level logLvl) {
         try {
-            // This block configure the logger with handler and formatter  
-            fh = new FileHandler("Server_Logger.xml", true);
+            String file_name = "";
+            switch (logLvl.getName()) {
+                case "SEVERE":
+                    file_name = SERVER_LOGGER_SEVERE_FILE;
+                    break;
+                case "WARNING":
+                    file_name = SERVER_LOGGER_WARNING_FILE;
+                    break;
+                case "INFO":
+                    file_name = SERVER_LOGGER_INFO_FILE;
+                    break;
+                default:
+                    file_name = SERVER_LOGGER_SEVERE_FILE;
+                    break;
+
+            }
+            fh = new FileHandler(file_name, true);
             LOGGER.addHandler(fh);
             SimpleFormatter formatter = new SimpleFormatter();
             fh.setFormatter(formatter);
 
-            // the following statement is used to log any messages  
-            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            LOGGER.log(logLvl, ex.getMessage(), ex);
+
             fh.close();
         } catch (SecurityException | IOException e) {
             Logger.getLogger(AppExceptionMapper.class.getName()).log(Level.SEVERE, null, e);
